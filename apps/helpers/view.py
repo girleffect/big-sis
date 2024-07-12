24ebc7c0ba354fb29b5bb060db0275a052d010c0
MatthewHowardRSA123 <Matthew.Howard@fullstack.co.za>
2024-07-12 10:02:40 +0200

    chore: Update data_switch view decorator to require GET instead of POST

--- apps/helpers/view.py
+++ apps/helpers/view.py
@@ -1,15 +1,15 @@
 from django.shortcuts import redirect
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_GET
 
-@require_POST
+@require_GET
 def data_switch(request):
     if 'moya' in request.session:
         del request.session['moya']
     else:   
         request.session['moya'] = True
     
-    if 'origin_path' in request.POST:
-        return redirect(request.POST['origin_path'])
+    if 'origin_path' in request.GET:
+        return redirect(request.GET['origin_path'])
     else:
         return redirect('/')
  
