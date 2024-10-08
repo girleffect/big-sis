2c0f876b04df5f32cab19d4a8e44e19ca28608ca
MatthewHowardRSA123 <Matthew.Howard@fullstack.co.za>
2024-10-08 13:17:03 +0200

    Refactor migration dependency in helpers app

--- apps/helpers/migrations/0011_auto_20241008_0207.py
+++ apps/helpers/migrations/0011_auto_20241008_0207.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('helpers', '0010_globalsettings_ga_measurement_id.py'),
+        ('helpers', '0010_globalsettings_ga_measurement_id'),
     ]
 
     operations = [
